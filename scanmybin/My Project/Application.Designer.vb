a0aac300ff081ff349e595bef7b53e5e6b97598c
0Fdemir <ssfenks@windowslive.com>
2018-05-26 18:53:52 +0300

    vbproj name changed.

--- scanmybin/My Project/Application.Designer.vb
+++ scanmybin/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.scanmybin.Main
+            Me.MainForm = Global.scanmybinVB.Main
         End Sub
     End Class
 End Namespace
